1c14f3b51dd6965a6d2499410ec315dd55dc702b
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-01-18 18:36:23 -0700

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
-const { webpack } = require("webpack");
+const  webpack = require("webpack");
 
+//webpack property entry output and mode 
 module.exports = {
     entry: './assets/js/script.js',
     output: {
