6bb8fdfa79525d7c20c596d799b14bea978e1ede
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-01-19 08:42:09 -0700

    modulized js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const path = require("path");
 const  webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 //webpack property entry output and mode 
 module.exports = {
+    //entry is the source for our webpack bundle 
     entry: './assets/js/script.js',
+    //tell webpack where to spit the output out to 
     output: {
+        //this creates a directory for the bundle 
         path: path.join(__dirname, 'dist'),
+        //filename can be anything you want it to be 
         filename: 'main.bundle.js',
     },
     plugins: [
@@ -13,6 +18,9 @@ module.exports = {
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", //report outputs to an HTML file in the dist folder
+        })
     ],
     mode: 'development'
  };
