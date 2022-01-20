b67e4c7f7dacbde93651e8dd3cb872b393fa6994
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-01-20 13:39:20 -0700

    webpack image loader

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,9 @@ module.exports = {
                                 return url.replace("../", "/assets")
                             }
                         }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
                     }
                 ]
             }
